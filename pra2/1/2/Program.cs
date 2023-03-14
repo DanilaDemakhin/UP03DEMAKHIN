eba909fdb9547792d1e4a65c280914accd5a54c8
Данька <don.demakhin2016@yandex.ru>
2023-03-14 11:24:49 +0300

    PR2
    PR2

---
+++ pra2/1/2/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace pr2_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CvUravnenie equation = new CvUravnenie(1, 3, -5);
+            equation.GetRoots();
+            Console.ReadKey(true);
+        }
+    }
+    class CvUravnenie
+    {
+        private double A;
+        private double B;
+        private double C;
+        private double D;
+        private double X1;
+        private double X2;
+        private double CalDiscr()
+        {
+            D = Math.Pow(B, 2) - 4 * A * C;
+            return D;
+        }
+        void CalculateRoots()
+        {
+            this.D = CalDiscr();
+            if (D > 0)
+            {
+               X1 = (-B + Math.Sqrt(D)) / (2 * A);
+               X2 = (-B - Math.Sqrt(D)) / (2 * A);
+               Console.WriteLine($"x1 = {X1}");
+               Console.WriteLine($"x2 = {X2}");
+                Console.WriteLine("УРА");
+            }
+            else if (D == 0)
+            {
+               X1 = -B / (2 * A);
+               Console.WriteLine($"X = {X1}");
+            }
+            else
+            {
+               Console.WriteLine("В уравнении нет корней! :(");
+            }
+        }
+        public void GetRoots()
+        {
+           CalculateRoots();
+        }
+        public CvUravnenie(double A, double B, double C)
+        {
+           this.A = A;
+           this.B = B;
+           this.C = C;
+        }
+    }
+}
---
+++ pra2/2/2_2/2_2/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace pr2_2
+{
+    class Num
+    {
+        internal class pr
+        {
+            static void Main(string[] args)
+            {
+                Num num = new Num();
+                int chislo;
+                for (int i = 1; i >= 0; i = num.GetNum(chislo) + 1)
+                {
+                    Console.WriteLine($"Бро, этот алгоритм просто пушка, введи число {i}");
+                    chislo = Convert.ToInt32(Console.ReadLine());
+                }
+                Console.ReadKey(true);
+            }
+        }
+        private int n = 0;
+        public int GetNum(int number)
+        {
+            if (SetNum(number))
+            {
+                this.n = number;
+            }
+            else
+            {
+                this.n = 0;
+            }
+            return this.n;
+        }
+        private bool SetNum(int number)
+        {
+            if (number == this.n + 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
